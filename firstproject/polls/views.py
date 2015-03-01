def1ddfe5b95a4c5af0b55a056ee06916392ffb1
CullyCross <elaugfein@gmail.com>
2015-03-01 16:30:08 +0200

    -Add tests, css and background image

--- firstproject/polls/views.py
+++ firstproject/polls/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 
 # Create your views here.
 from django.shortcuts import render
+from django.utils import timezone
 from django.views import generic
 from models import Question, Choice
 
@@ -14,7 +15,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
